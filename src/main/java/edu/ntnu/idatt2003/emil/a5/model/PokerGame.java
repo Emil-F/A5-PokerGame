b46cd68888b311051cc4888a2a7ddf48f9764037
Emil-F <e.fagerjord@gmail.com>
2026-03-11 14:59:50 +0100

    feat: added template for PokerGame within the class diagram

--- src/main/java/edu/ntnu/idatt2003/emil/a5/model/PokerGame.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/model/PokerGame.java
@@ -3,16 +3,61 @@ package edu.ntnu.idatt2003.emil.a5.model;
 import edu.ntnu.idatt2003.emil.a5.model.users.Bot;
 import edu.ntnu.idatt2003.emil.a5.model.users.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Poker {
+public class PokerGame {
   private final DeckOfCards deck;
   private Player player;
-  private List<Bot> bots;
+  private final List<Bot> bots;
+  private final List<PlayingCard> communityCards;
+  // Todo: implement pot system
+  // private int pot;
   private int round;
 
-  public Poker() {
+  public PokerGame() {
     this.deck = new DeckOfCards();
+    this.player = new Player("Emil", 50000);
+    this.bots = new ArrayList<>();
+    this.communityCards = new ArrayList<>();
     this.round = 1;
   }
+
+  public DeckOfCards getDeck() {
+    return deck;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public List<Bot> getBots() {
+    return bots;
+  }
+
+  public List<PlayingCard> getCommunityCards() {
+    return communityCards;
+  }
+
+  public int getRound() {
+    return round;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
+
+  public void populateBots(int botCount) {
+    for(int i = 1; i <= botCount; i++) {
+      this.bots.add(new Bot("Bot" + i));
+    }
+  }
+
+  public void startGame() {}
+
+  public void stopGame() {}
+
+  public void dealCards() {}
+
+  public void determineWinner() {}
 }
