398a1b61e1fd8078aea61842252da5785ca3a1a3
Emil-F <e.fagerjord@gmail.com>
2026-03-14 17:42:37 +0100

    feat: added views for the PlayerView, CasinoView

--- src/main/java/edu/ntnu/idatt2003/emil/a5/view/HeaderView.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/view/HeaderView.java
@@ -1,11 +1,14 @@
 package edu.ntnu.idatt2003.emil.a5.view.sub;
 
 import edu.ntnu.idatt2003.emil.a5.controller.PokerController;
+import edu.ntnu.idatt2003.emil.a5.model.PlayingCard;
 import edu.ntnu.idatt2003.emil.a5.view.MainView;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class Header extends StackPane {
   private final PokerController controller;
@@ -15,7 +18,9 @@ public class Header extends StackPane {
     setId("header");
     getStylesheets().add(getClass().getResource("/css/header.css").toExternalForm());
     setAlignment(Pos.CENTER);
-    getChildren().add(createButtonRow());
+    Text title = new Text("PokerGame");
+    title.setId("title");
+    getChildren().add(title);
   }
 
   public HBox createButtonRow() {
@@ -30,7 +35,7 @@ public class Header extends StackPane {
 
   public Button createHomeBtn() {
     Button homeBtn = new Button("Home");
-    homeBtn.setOnAction(e -> controller.addCommunityCard());
+    homeBtn.setOnAction(e -> controller.startGame());
     return homeBtn;
   }
 }
