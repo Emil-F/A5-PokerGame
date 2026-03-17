9bf6348a7de5cada8f08f3acdd7efbc7eb455732
Emil-F <e.fagerjord@gmail.com>
2026-03-17 01:23:01 +0100

    feat: added functionality for going through the rounds of poker and determining the winner of the round

--- src/main/java/edu/ntnu/idatt2003/emil/a5/model/poker/round/PokerRound.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/model/poker/round/PokerRound.java
@@ -1,29 +1,34 @@
 package edu.ntnu.idatt2003.emil.a5.model.poker.round;
 
-import edu.ntnu.idatt2003.emil.a5.model.PlayerAction;
-import edu.ntnu.idatt2003.emil.a5.model.PlayingCard;
-import edu.ntnu.idatt2003.emil.a5.model.users.User;
+import edu.ntnu.idatt2003.emil.a5.model.poker.PlayingCard;
+import edu.ntnu.idatt2003.emil.a5.model.poker.hand.HandCheckResult;
+import edu.ntnu.idatt2003.emil.a5.model.users.Bot;
+import edu.ntnu.idatt2003.emil.a5.model.users.Participant;
 import edu.ntnu.idatt2003.emil.a5.model.poker.hand.HandChecker;
+import edu.ntnu.idatt2003.emil.a5.model.users.Player;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.logging.Logger;
 
 public class PokerRound {
+  private final Logger logger = Logger.getLogger(PokerRound.class.getName());
+
   private final RoundState roundState;
   private final List<PlayingCard> communityCards;
   private final DeckOfCards deck;
   private final Pot pot;
-  private final BettingRound bettingRound;
+  private BettingRound bettingRound;
   private final HandChecker handChecker;
-  private List<User> players;
-  private User currentPlayer;
+  private List<Participant> players;
 
-  public PokerRound(List<User> players) {
+  private HandCheckResult winningHandCheckResult;
+
+  public PokerRound(Player player, List<Bot> bots, List<Participant> players) {
     this.roundState = new RoundState();
     this.communityCards = new ArrayList<>();
     this.deck = new DeckOfCards();
     this.pot = new Pot();
-    this.bettingRound = new BettingRound();
+    this.bettingRound = new BettingRound(player, bots);
     this.handChecker = new HandChecker();
     this.players = players;
   }
@@ -34,6 +39,20 @@ public class PokerRound {
 
   public List<PlayingCard> getCommunityCards() {
     return communityCards;
+  }
+
+  public String getCommunityCardsString() {
+    StringBuilder communityCardsString = new StringBuilder();
+    communityCardsString.append("Community Cards: [");
+    for (int i = 0; i < getCommunityCards().size(); i++) {
+      if (i == getCommunityCards().size() - 1) {
+        communityCardsString.append(getCommunityCards().get(i).getAsString());
+        continue;
+      }
+      communityCardsString.append(getCommunityCards().get(i).getAsString()).append(", ");
+    }
+    communityCardsString.append("]");
+    return communityCardsString.toString();
   }
 
   public DeckOfCards getDeck() {
@@ -52,13 +71,17 @@ public class PokerRound {
     return handChecker;
   }
 
+  public HandCheckResult getLastWinner() {
+    return winningHandCheckResult;
+  }
+
   public void advanceRoundState() {
     roundState.advanceRound();
   }
 
   public void resetCards() {
     communityCards.clear();
-    for (User player : players) {
+    for (Participant player : players) {
       player.clearCards();
     }
   }
@@ -66,34 +89,41 @@ public class PokerRound {
   public void dealPreFlop() {
     resetCards();
     dealHoleCards(this.players);
+    bettingRound.start(players);
   }
 
   public void dealFlop() {
-    advanceRoundState();
     communityCards.addAll(deck.dealHand(3));
   }
 
   public void dealTurn() {
-    advanceRoundState();
     communityCards.addAll(deck.dealHand(1));
   }
 
   public void dealRiver() {
-    advanceRoundState();
     communityCards.addAll(deck.dealHand(1));
   }
 
-  public void handleAction(User user, PlayerAction action) {
-    bettingRound.handleAction(user, action);
+  public void handleAction(Participant participant, PlayerAction action) {
+    bettingRound.handleAction(participant, action);
   }
 
   public void resolveShowdown() {
-    advanceRoundState();
+    List<HandCheckResult> checkResults = new ArrayList<>();
+    for (Participant player : players) {
+      checkResults.add(this.handChecker.checkHand(player, this.communityCards));
+    }
+
+    checkResults.sort(Comparator.comparing(HandCheckResult::cardTotal).reversed());
+    checkResults.sort(Comparator.comparing(HandCheckResult::rank));
+    logger.info(checkResults.toString());
+    logger.info(checkResults.getFirst().participant().getName() + " wins with a [" + checkResults.getFirst().rank() + "] and card value of [" + checkResults.getFirst().cardTotal() + "]");
+    winningHandCheckResult = checkResults.getFirst();
   }
 
-  public void dealHoleCards(List<User> participants) {
+  public void dealHoleCards(List<Participant> participants) {
     this.deck.restockCards();
-    for (User participant : participants) {
+    for (Participant participant : participants) {
       participant.setCards(deck.dealHand(2));
     }
   }
